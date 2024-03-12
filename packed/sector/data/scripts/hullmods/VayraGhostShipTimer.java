1d7af24632f547b94a68c30ae3b05c983c3de070
RkShaRkz <darksidemetatron@gmail.com>
2024-03-12 15:08:54 +0100

    Get rid of the 'packed' folder
    While the packed/FINAL was a decent idea, it turns out that any tool other than Notepad++
    either breaks CSVs or somehow screws up something. So, while it was good for a general overview of
    "what else is there to pull in" it proved much better and time-saving to just pull directly to
    src/data instead, and commit after verifying.
    
    tl;dr the automerge sucked, theres no need for this folder to exist anymore - it's done it's purpose.

--- packed/sector/data/scripts/hullmods/VayraGhostShipTimer.java
+++
@@ -1,9 +0,0 @@
-package data.scripts.hullmods;
-
-import com.fs.starfarer.api.combat.BaseHullMod;
-
-public class VayraGhostShipTimer extends BaseHullMod {
-
-    // doo dee doo dee doo dee doo
-    
-}
