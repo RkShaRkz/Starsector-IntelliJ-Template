47a1ca5fb33961daff6e1a1322660a4955bdf6fe
RkShaRkz <darksidemetatron@gmail.com>
2024-07-15 05:20:35 +0200

    Resolve issues #18 #19 #20 - ConcurrentModificationException
    Ok, these three issues were all duplicates of the same issue.
    On the other hand, the way this code was written, this crash would
    happen 100% of the time, and is a legitimate textbook case of a
    ConcurrentModificationException, where items were being removed
    while the list was being iterated over.
    
    Switching over to an iterator and removing via iterator should
    fix the issue.
    As reported by the immediate beta-tester - it resolved the issue.

--- src/data/scripts/campaign/intel/VayraPersonBountyManager.java
+++ src/data/scripts/campaign/intel/VayraPersonBountyManager.java
@@ -73,13 +73,16 @@ public class VayraPersonBountyManager extends BaseEventManager {
 
     private void checkForFuckedUpParticipants() {
         checkedForFuckedUpParticipants = true;
-        for (String fid : getSharedData().getParticipatingFactions()) {
+//        for (String fid : getSharedData().getParticipatingFactions()) {
+        for (Iterator<String> iter = getSharedData().getParticipatingFactions().iterator(); iter.hasNext(); ) {
+            String fid = iter.next();
             if (Global.getSector().getFaction(fid) == null) {
                 Global.getSector().getCampaignUI().addMessage(fid + " is an invalid bounty participant, please yell at its mod author", Color.red);
                 Global.getSector().getCampaignUI().addMessage(fid + " bounties will not function until they fix it", Color.red);
                 Global.getSector().getCampaignUI().addMessage("i could have crashed the game right here, i SHOULD have crashed the game right here", Color.red);
                 Global.getSector().getCampaignUI().addMessage("god knows you'd fuckin' deserve it", Color.red);
-                getSharedData().removeParticipatingFaction(fid);
+//                getSharedData().removeParticipatingFaction(fid);
+                iter.remove();
             }
         }
     }
