84caff3a1f5efa8b34a64446d3870a9fa114ef2c
RkShaRkz <darksidemetatron@gmail.com>
2024-10-09 23:42:30 +0200

    Prevent (n)exerelin from spawning colonial factions too early (#54)
    * Prevent (n)exerelin from spawning colonial factions too early
    
    Some of Vayra factions were made/designed to happen later on in the game, e.g.
    around cycle 212. However, due to (n)exerelin's faction respawn system, it ends
    up respawning them around cycle 206 before they've even made their first appearance.
    
    This makes them usually pick a subpar planet and prevent them from doing whatever they
    were supposed to be doing when they made their 'official' happening later on.
    
    The fix for this, as per Histidine's suggestion, is to add them to SectorManager's
    DO_NOT_RESPAWN_FACTIONS set, so that's what's been done.
    
    * Removed kadur_remnant from the 'do not respawn' Nex faction list
    
    * Add the other 5 factions to DO_NOT_RESPAWN list only if their relevant features are enabled
    
    * Latest JAR

--- src/data/scripts/VayraMergedModPlugin.java
+++ src/data/scripts/VayraMergedModPlugin.java
@@ -152,6 +152,16 @@ public class VayraMergedModPlugin extends BaseModPlugin {
             for (String factionId : VayraColonialManager.loadColonyFactionList()) {
                 setExerelinActive(factionId, COLONIAL_FACTIONS_ENABLED);
             }
+            // Make Exerelin/Nexerelin *not* respawn our own factions
+            // Since these factions are supposed to make an appearance later on in the game, Nexerelin
+            // thinks they're deceased and tries bringing them back *much earlier* than they would actually happen
+            //
+            // This makes them (usually) pick a shitty planet and have zero chances of doing whatever they were supposed
+            // to be doing.
+            //
+            // So instead of them coming in at cycle 212, exerelin usually 'respawns' them at cycle 206.
+            // Hopefully this fix makes (n)exerelin stop 'respawning' them before they even spawn for the first time.
+            makeExerelinIgnoreVayraFactions();
         }
 
         if (HAVE_LUNALIB) {
@@ -446,6 +456,32 @@ public class VayraMergedModPlugin extends BaseModPlugin {
                     DiplomacyManager.disallowedFactions.add(conf.factionId);
                 }
 
+            }
+        }
+    }
+
+    private void makeExerelinIgnoreVayraFactions() {
+        if (EXERELIN_LOADED) {
+            /**
+             * communist_clouds
+             * science_fuckers
+             * warhawk_republic
+             * almighty_dollar
+             * ashen_keepers
+             * kadur_remnant
+             */
+            // If POPULAR_FRONT is enabled, make the 'communist clouds' faction not respawn
+            // since it's POPULAR_FRONT-related
+            if (VayraMergedModPlugin.POPULAR_FRONT_ENABLED) {
+                SectorManager.DO_NOT_RESPAWN_FACTIONS.add("communist_clouds");
+            }
+            // If COLONIAL_FACTIONS is enabled, make the 'colonial factions' not respawn
+            // since Nex breaks their scripted spawn by 'respawning' them too early
+            if (VayraMergedModPlugin.COLONIAL_FACTIONS_ENABLED) {
+                SectorManager.DO_NOT_RESPAWN_FACTIONS.add("science_fuckers");
+                SectorManager.DO_NOT_RESPAWN_FACTIONS.add("warhawk_republic");
+                SectorManager.DO_NOT_RESPAWN_FACTIONS.add("almighty_dollar");
+                SectorManager.DO_NOT_RESPAWN_FACTIONS.add("ashen_keepers");
             }
         }
     }
