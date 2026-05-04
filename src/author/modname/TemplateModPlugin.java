24cf9907a39e2193ac3812771e2106b2af4ee2b9
Stackwyre <stackwyre@gmail.com>
2026-05-04 18:24:46 -0500

    Fix #98: [Issue] Split the bounty duration for "player catchable bounties" and "bounties

--- src/author/modname/TemplateModPlugin.java
+++ src/author/modname/TemplateModPlugin.java
@@ -4,19 +4,22 @@ import com.fs.starfarer.api.BaseModPlugin;
 import com.fs.starfarer.api.Global;
 
 public class TemplateModPlugin extends BaseModPlugin {
+    
+    public static final int BOUNTY_DURATION = 365;
+    public static final int PLAYER_BOUNTY_DURATION = 36500;
+    
     @Override
     public void onApplicationLoad() throws Exception {
         super.onApplicationLoad();
-
-        // Test that the .jar is loaded and working, using the most obnoxious way possible.
-        throw new RuntimeException("Template mod loaded!\nRemove this crash in TemplateModPlugin.");
     }
-
+    
     @Override
     public void onNewGame() {
         super.onNewGame();
-        // Add your code here, or delete this method (it does nothing unless you add code)
     }
-
-    // You can add more methods from ModPlugin here. Press Control-O in IntelliJ to see options.
+    
+    @Override
+    public void onGameLoad(boolean newGame) {
+        super.onGameLoad(newGame);
+    }
 }
