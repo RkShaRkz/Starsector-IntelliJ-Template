3da2038390ea2027a62ba952fcaecff321ac534b
RkShaRkz <darksidemetatron@gmail.com>
2026-05-08 00:19:56 +0200

    Issue #95 - Sanitize FleetDoctrine's commander/officer skills before spawning bounty fleet (#97)
    I'm getting pissed at these random ass crashes while i'm trying to test something else, so it's time to put a stop to this once and for all.
    
    Instead of using reflection to somehow replace the FactionAPI's FleetDoctrineAPI's commander/officer skills (which are a list of strings) lets instead rely on the `FleetParamsV3.doctrineOverride` to do that instead of us.
    
    So, at the start of the `spawnFleet()` method, ~~sanitize~~ scrutinize the Fleet's doctrine's skills against allowed/existing/valid skills, and if any invalid ones are found - create a wrapper SkillSanitizingDoctrine, wrapping the original doctrine.
    
    Everything but the `getCommanderSkills()` and `getOfficerSkills()` will be delegated to the original doctrine, and these two methods will actually just delete the offending skills from the original (json?) skill list.
    
    Should work I think? Time to test it out.
    
    Issue link:
    https://github.com/RkShaRkz/Starsector-IntelliJ-Template/issues/95
    
    ---------
    
    
    * Issue #95 - Sanitize FleetDoctrine's commander/officer skills before spawning bounty fleet
    
    I'm getting pissed at these random ass crashes while i'm trying to test something else, so it's time to
    put a stop to this once and for all.
    
    Instead of using reflection to somehow replace the FactionAPI's FleetDoctrineAPI's commander/officer skills
    (which are a list of strings) lets instead rely on the `FleetParamsV3.doctrineOverride` to do that instead of us.
    
    So, at the start of the `spawnFleet()` method, ~~sanitize~~ scrutinize the Fleet's doctrine's skills
    against allowed/existing/valid skills, and if any invalid ones are found - create a wrapper SkillSanitizingDoctrine,
    wrapping the original doctrine.
    
    Everything but the `getCommanderSkills()` and `getOfficerSkills()` will be delegated to the original doctrine,
    and these two methods will actually just delete the offending skills from the original (json?) skill list.
    
    Should work I think? Time to test it out.
    
    Issue link:
    https://github.com/RkShaRkz/Starsector-IntelliJ-Template/issues/95
    
    * Address own CR comments
    
    * Touchup to SkillSanitizingDoctrine, latest JAR
    
    Now we have a concrete proof that it actually works fine.
    ```
    483405 [Thread-2] ERROR data.scripts.campaign.intel.VayraPersonBountyIntel  - Faction: luddic_church contains invalid skills in it's doctrine! Using doctrine override! override: data.scripts.campaign.intel.SkillSanitizingDoctrine@2ccf6209
    483408 [Thread-2] WARN  data.scripts.campaign.intel.SkillSanitizingDoctrine  - Combined check failed while testing skill: fighter_doctrine, removing it!
    483412 [Thread-2] WARN  data.scripts.campaign.intel.SkillSanitizingDoctrine  - Combined check failed while testing skill: asdafgasf, removing it!
    483418 [Thread-2] INFO  data.scripts.campaign.intel.VayraPersonBountyIntel  - Starting person bounty posted by faction [Luddic Church] for person [Andrea Cooper] from faction [Diable Avionics]
    ```
    
    no crashes happened.
    
    Sure, i 'injected' the bad skills through the SkillSanitizingDoctrine as well as modifying the `VayraPersonBountyIntel` to use an
    empty list for a list of valid skill IDs (so that all skills are considered invalid and trigger creation of SkillSanitizingDoctrine)
    but all in all - things *should* work great. I think.
    
    Perhaps I should test with an actually broken faction but... too lazy. Lets fix that the next time it happens, if it happens.
    
    * Touch up a lying log and latest JAR
    
    It was saying "prepare to crash" when no crash should happen.
    Slightly modified the log to not lie so much; it should just say
    `prepare to override with skill sanitizer doctrine ...` now.

--- src/data/scripts/campaign/intel/VayraPersonBountyIntel.java
+++ src/data/scripts/campaign/intel/VayraPersonBountyIntel.java
@@ -39,6 +39,7 @@ import com.fs.starfarer.api.util.WeightedRandomPicker;
 import data.domain.PersonBountyEventDataRepository;
 import data.scripts.util.MiscUtils;
 import org.apache.log4j.Logger;
+import org.jetbrains.annotations.Nullable;
 
 import java.awt.*;
 import java.util.List;
@@ -117,6 +118,10 @@ public final class VayraPersonBountyIntel extends BaseIntelPlugin implements Eve
 
     public synchronized PersonBountyEventData getPersonBountyEventDataFromRepository() {
         return PersonBountyEventDataRepository.getInstance().getPersonBountyEventData();
+    }
+
+    private List<String> getListOfAllExistingSkillIds() {
+        return Global.getSettings().getSkillIds();
     }
 
     public VayraPersonBountyIntel() {
@@ -989,7 +994,50 @@ public final class VayraPersonBountyIntel extends BaseIntelPlugin implements Eve
         }
     }
 
+    /**
+     * Method planned to sanitize the faction's doctrine's commander skills but since we cannot quite do that,
+     * it just returns an 'optional' factrine override to be used in FleetParamsV3
+     *
+     * @param faction faction whose doctrine should be sanitized of invalid skills
+     * @return whether we should proceed with the passed 'faction' or not, since the faction's commander
+     * or officer skills contain invalid skills.
+     * <b>null</b> indicates that no problems related to skills should happen.
+     */
+    private @Nullable FactionDoctrineAPI sanitizeFactionDoctrine(FactionAPI faction) {
+        SkillSanitizingDoctrine retVal = null;
+        boolean hasBadSkills = false;
+        List<String> validSkills = getListOfAllExistingSkillIds();
+        FactionDoctrineAPI doctrine = faction.getDoctrine();
+
+        for (String skill : doctrine.getCommanderSkills()) {
+            if (!validSkills.contains(skill)) {
+                hasBadSkills = true;
+                log.error("Faction: " + faction.getId() + " contains no longer existing skills in it's doctrine! Invalid COMMANDER skill: [" + skill + "], prepare to override with skill sanitizer doctrine ...");
+            }
+        }
+
+        for (String skill : doctrine.getOfficerSkills()) {
+            if (!validSkills.contains(skill)) {
+                hasBadSkills = true;
+                log.error("Faction: " + faction.getId() + " contains no longer existing skills in it's doctrine! Invalid OFFICER skill: [" + skill + "], prepare to override with skill sanitizer doctrine ...");
+            }
+        }
+
+        // Update the doctrine with the sanitized skill list
+//        doctrine.setCommanderSkills(sanitizedSkills);
+        if (hasBadSkills) {
+            retVal = new SkillSanitizingDoctrine(doctrine);
+        }
+
+        return retVal;
+    }
+
     private void spawnFleet() {
+        // First thing's first, since some bounty factions might still reference no-longer-existing skills,
+        // lets first sanitize their doctrine before proceeding...
+        FactionDoctrineAPI potentialDoctrineOverride = sanitizeFactionDoctrine(bountyFaction);
+
+        // Now, carry on like we did before ...
         String fleetFactionId = bountyFaction.getId();
         if (bountyType == BountyType.DESERTER) {
             fleetFactionId = faction.getId();
@@ -1025,6 +1073,11 @@ public final class VayraPersonBountyIntel extends BaseIntelPlugin implements Eve
                 0f, // utilityPts
                 0f // qualityMod
         );
+        // If we received a non-null potential skill sanitizing doctrine override, set it!
+        if (potentialDoctrineOverride != null) {
+            log.error("Faction: " + faction.getId() + " contains invalid skills in it's doctrine! Using doctrine override! override: " + potentialDoctrineOverride);
+            params.doctrineOverride = potentialDoctrineOverride;
+        }
 
         if (VAYRA_DEBUG) {
             log.info("generated FleetParams for location: " + hideoutLocation.getLocationInHyperspace() + ", fleetFactionId: " + fleetFactionId + ", combatPoints: " + fp + "\t = " + params);
